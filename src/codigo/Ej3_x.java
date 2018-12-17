563fe866aacb315825d8452eb9c0bcb63189bb91
PlasmaXIII <44767290+SantosCJ98@users.noreply.github.com>
2018-12-17 22:50:11 +0100

    Commit 2

---
+++ src/codigo/Ej3_x.java
@@ -0,0 +1,39 @@
+package codigo;
+
+public class Ej3_x {
+
+	public static void main(String[] args) {
+		
+		int [] miarray = {5};
+		
+		if (miarray[0] != referencia(miarray)[0]) {
+			
+			System.out.println("Las referencias cambian");
+			
+		}
+		
+		if (miarray[0] == valor(3, miarray)[0]) {
+			
+			System.out.println("Los valores no");
+			
+		}
+
+	}
+	
+	public static int [] referencia (int [] miarray) {
+		
+		miarray[0] = 3;
+		
+		return miarray;
+		
+	}
+	
+	public static int [] valor (int valor, int [] miarray) {
+		
+		miarray[0] = valor;
+		
+		return miarray;
+		
+	}
+
+}
